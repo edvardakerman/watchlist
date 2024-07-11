8469eeb392965f50a9fc6eafcd7bb6f585ba52af
edvardakerman <ed@sagovagen.se>
2024-07-12 00:04:48 +0200

    show case

---
+++ app/components/MovieShowCase.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import React from "react";
+import { Movie } from "../models/movie";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+interface MovieProps {
+    movies: Movie[];
+    title: string;
+}
+
+export default function MovieShowCase({ movies, title }: MovieProps) {
+    const topTwelve = movies.slice(0, 12);
+
+    return (
+        <div className="py-10">
+            <Link href='#'><h3 className='text-3xl font-bold'>{title}</h3></Link>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 mt-8 gap-6">
+                {topTwelve.map((movie) => (
+                    <div className="relative group">
+                        <Image className="hover:brightness-50" title={movie.title} width={200} height={100} alt="movie poster" src={`https://image.tmdb.org/t/p/w500/` + `${movie.poster_path}`}></Image>
+                        <div className="absolute bottom-0 left-0 w-full p-2 text-white text-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                            {movie.title}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
--- app/home/explore/page.tsx
+++ app/home/explore/page.tsx
@@ -1,25 +1,38 @@
 import React from 'react';
-import TrendingMovies from '@/app/components/TrendingMovies';
+import MovieShowCase from '@/app/components/MovieShowCase';
 
-async function getData() {
-    const res = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&language=en-US&sort_by=popularity.desc`);
+async function getData(endpoint: string) {
+    // endpoints: ['now_playing', 'popular', 'top_rated', 'upcoming'];
+    const res = await fetch(`https://api.themoviedb.org/3/movie/${endpoint}?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1&include_adult=true`);
 
     if (!res.ok) {
-      throw new Error('Failed to fetch data')
+        throw new Error('Failed to fetch data')
     }
-   
+
     return res.json()
-  }
+}
 
 
 export default async function ExplorePage() {
-    const data = await getData()
-    const trending = data.results;
+    let data = await getData('now_playing')
+    const now_playing = data.results;
+
+    data = await getData('popular')
+    const popular = data.results;
+
+    data = await getData('top_rated')
+    const top_rated = data.results;
+
+    data = await getData('upcoming')
+    const upcoming = data.results;
 
     return (
         <div>
-            <h1>Trending Movies</h1>
-            <TrendingMovies movies={trending} />
+            <h1 className='text-5xl font-bold my-10'>Explore Movies</h1>
+                <MovieShowCase title='Popular Movies' movies={popular} />
+                <MovieShowCase title='Upcoming Movies' movies={upcoming} />
+                <MovieShowCase title='Top Rated Movies' movies={top_rated} />
+                <MovieShowCase title='Now in Theaters' movies={now_playing} />
         </div>
     );
 }
