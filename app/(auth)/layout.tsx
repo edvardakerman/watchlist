c50cb8671af7edbf499c040946222e5910689b22
edvardakerman <ed@sagovagen.se>
2024-08-01 12:11:10 +0200

    fill warning fix

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,17 +6,19 @@ import Footer from "../components/Footer";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col h-[calc(100dvh)] justify-between">
+    <div className="relative flex flex-col h-[calc(100dvh)] justify-between">
+      <div className="absolute inset-0 -z-10">
+        <Image
+          src={BackgroundImage}
+          alt="background image"
+          className="flex object-cover -z-10 brightness-50"
+          priority
+          fill
+        />
+      </div>
       <Navbar bg="bg-transparent" />
       <main className="w-full max-w-7xl mx-auto px-5 sm:px-6 lg:px-8 mt-20">
         <div className="flex flex-col items-center justify-center bg-transparent">
-          <Image
-            src={BackgroundImage}
-            alt="background image"
-            className="flex object-cover -z-10 brightness-50"
-            priority
-            fill
-          />
           {children}
         </div>
       </main>
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { NextAuthProvider } from "./components/NextAuthProvider";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import { MoviesProvider } from "./context/MovieContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
